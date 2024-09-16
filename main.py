722387b121328c964e982181326d238bd051b767
DSkvortsova <dkvarts03155@mail.ru>
2024-09-16 14:16:52 +0300

    v 2.0

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import streamlit as st
 import requests
 
-def click_button(value):
+def click_button_post(value):
      requests.post("http://127.0.0.1:8000/items", json={"text": value})
 
 st.title('Первая лабораторная работа v1.0')
-text = st.text_input('Введите данные, для проверки работоспособности')
+text = st.text_input('Введите данные, для проверки работоспособности и нажмите на кнопку')
 if st.button("Отправить", type="secondary"):
-     on_click = click_button(text)
+     on_click = click_button_post(text)
      st.rerun()
+
+def click_button_get():
+     value = requests.get("http://127.0.0.1:8000/items")
+     return value.json()
+
+st.title('Первая лабораторная работа v2.0')
+
+st.text('Нажмите на кнопку, чтобы увидить данные с сервера')
+if st.button ("Получить", type ="primary"):
+     text = click_button_get()
+     st.info(text)
 
